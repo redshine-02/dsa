47150086d4c3e1c4bdbdd09da8a57ddfd0531f17
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-01-12 02:25:23 +0530

    updates

--- Recursion/Backtracking/floodfill.java
+++ Recursion/Backtracking/floodfill.java
@@ -7,3 +7,80 @@ to get to the best path*/
                                               L=left
                                               D=down
                                               R=Right */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        //boolean[][] viz= new boolean[n][m];
+        floodfill(arr, 0, 0, "",viz);
+    }
+
+//METHOD 1
+    
+    // asf -> answer so far
+    // public static void floodfill(int[][] maze, int sr, int sc, String asf,boolean[][] viz) {
+    //     if(sr<=maze.length || sc<=maze[0].length || sr>0 || sc<0 || maze[sr][sc]==1 || viz[sr][sc] == true){
+    //         return;
+    //     }
+    //     if(sr==maze.length-1 && sc==maze[0].length-1){
+    //         System.out.println(asf);
+    //         return;
+    //     }
+    //     viz[sr][sc]=true;
+    //     floodfill(maze,sr,sc-1,asf+"t",viz);
+    //     floodfill(maze,sr-1,sc,asf+"l",viz);   
+    //     floodfill(maze,sr,sc+1,asf+"d",viz);
+    //     floodfill(maze,sr+1,sc,asf+"r",viz);
+    //     viz[sr][sc]=false;
+    // }
+
+//METHOD 2
+
+    // public static void floodfill(int[][] maze, int sr, int sc, String asf) {
+    //     if(sr<=maze.length || sc<=maze[0].length || sr>0 || sc<0 || maze[sr][sc]==1){
+    //         return;
+    //     }
+    //     if(sr==maze.length-1 && sc==maze[0].length-1){
+    //         System.out.println(asf);
+    //         return;
+    //     }
+    //     maze[sr][sc]=1;
+    //     floodfill(maze,sr,sc-1,asf+"t");
+    //     floodfill(maze,sr-1,sc,asf+"l");   
+    //     floodfill(maze,sr,sc+1,asf+"d");
+    //     floodfill(maze,sr+1,sc,asf+"r");
+    //     maze[sr][sc]=0;
+    // }
+
+//METHOD 3
+        static int[][] dir = {{0,-1},{-1,0},{0,1},{1,0}};
+        static String[] dirN ={"t","l","d","r"};
+        public static void floodfill(int[][] maze, int sr, int sc, String asf) {
+        if(sr<=maze.length || sc<=maze[0].length || sr>0 || sc<0 || maze[sr][sc]==1 || viz[sr][sc] == true){
+            return;
+        }
+        if(sr==maze.length-1 && sc==maze[0].length-1){
+            System.out.println(asf);
+            return;
+        }
+        maze[sr][sc]=1;
+        for(int d=0;d<dir.length;d++){
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+            floodfill(maze, r, c, asf+dirN[d]);
+        }
+        maze[sr][sc]=0;
+    }
+}
---
+++ Recursion/Backtracking/target_sum_subset.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class target_sum_subset {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr =new int[n];
+        for(int i = 0 ; i<n ; i++){
+            arr[i]=sc.nextInt();
+        }
+        int t = sc.nextInt();
+        printTargetSumSubsets(arr, 0, "", 0, t);
+
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String asf, int sum, int tar) {
+        if(sum>tar){
+            return;
+        }
+
+        if(idx==arr.length){
+            if(sum==tar){
+                System.out.println(asf);
+            }
+            return;
+        }
+        //print with
+        printTargetSumSubsets(arr, idx+1, asf+arr[idx]+",", sum+arr[idx], tar);
+
+        //print without
+        printTargetSumSubsets(arr, idx+1, asf, sum, tar);
+    }
+
+}
