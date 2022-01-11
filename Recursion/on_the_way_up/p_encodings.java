773fd862caef40a0e49fd1681437cf321c6491f9
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-01-11 23:34:19 +0530

    updates

--- Recursion/on_the_way_up/p_encodings.java
+++ Recursion/on_the_way_up/p_encodings.java
@@ -1,3 +1,33 @@
-public class p_encodings {
+import java.util.*;
+
+public class Main {
     
+    public static void main(String args[])throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        encoding(str , "");
+    }
+
+    public static void encoding(String s , String ans){
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        if(s.charAt(0)=='0'){
+            return;
+        }
+
+        char ch1 = s.charAt(0);
+        int v1 = ch1 - '0';
+        encoding(s.substring(1), ans+(char)(v1+'a'-1));
+
+        if(s.length()>1){
+            char ch2 = s.charAt(1);
+            int v2 = ch2 - '0';
+            int num = v1*10+v2;
+            if(num<=26){
+            encoding(s.substring(2), ans+(char)(num+'a'-1));}
+        }
+    }
 }
--- Recursion/on_the_way_up/p_permutations.java
+++ Recursion/on_the_way_up/p_permutations.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class p_permutations {
-    
+
+    public static void main(String args[])throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        printperm(str,"");
+    }
+    public static void printperm(String s , String ans){
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            String res = s.substring(0,i) + s.substring(i+1);
+            printperm(res, ans+ch);
+        }
+    }
 }
