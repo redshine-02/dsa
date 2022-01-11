33bdc0bcc2ed047747c58dbc9d84d12423bde414
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-01-11 19:49:07 +0530

    updates

--- Recursion/on_the_way_up/p_mazepath_jump.java
+++ Recursion/on_the_way_up/p_mazepath_jump.java
@@ -1,3 +1,35 @@
-public class p_mazepath_jump {
-    
-}
+import java.io.*;
+	import java.util.*;
+
+	public class Main {
+
+	    public static void main(String[] args) throws Exception {
+			Scanner sc = new Scanner(System.in);
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			printMazePaths( 0 , 0 , n-1 , m-1 , "");
+
+	    }
+
+	    // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        if(sr==dr && sc==dc){
+				System.out.println(psf);
+				return;
+			}
+			for(int jump = 1; sc+jump<=dc;jump++){
+				printMazePaths( sr , sc+jump , dr , dc , psf+"h"+jump);
+			}
+			for(int jump = 1; sr+jump<=dr;jump++){
+				printMazePaths( sr+jump , sc , dr , dc , psf+"v"+jump);
+			}
+            for(int jump =1;sr+jump<=dr && sc+jump<=dc;jump++){
+				printMazePaths( sr+jump , sc+jump , dr , dc , psf+"d"+jump);
+			}
+
+	    }
+
+	}
--- Recursion/on_the_way_up/p_stairs.java
+++ Recursion/on_the_way_up/p_stairs.java
@@ -1,3 +1,29 @@
+import java.io.*;
+import java.util.*;
+
 public class p_stairs {
-    
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStairPaths(n,"");
+
+    }
+
+    public static void printStairPaths(int n, String path) {
+        if(n==0){
+            System.out.println(path);
+            return;
+        }
+        if(n-1>=0){
+            printStairPaths(n-1,path+"1");
+        } 
+        if(n-2>=0){
+            printStairPaths(n-2,path+"2");
+        }
+        if(n-3>=0){
+            printStairPaths(n-3,path+"3");
+        }
+    }
+
 }
