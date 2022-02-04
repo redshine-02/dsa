612fb47e92ccd72389d23b863aedf355ce36304a
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-02-04 22:50:19 +0530

    goldmine updated

--- DP/goldmine.java
+++ DP/goldmine.java
@@ -2,22 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class goldmine {
-    public static int goldm(int i, int j, int[][] grid, int[][] dp){
-        if(i<0 ||i>=grid.length || j>=grid[0].length)
-         return 0; 
-        if(dp[i][j]!=0){
-             return dp[i][j];
-         }
-         int op1=goldm(i-1,j+1,grid,dp);
-         int op2=goldm(i,j+1,grid,dp);
-         int op3=goldm(i+1,j+1,grid,dp);
-
-         int max= Math.max(op1,Math.max(op2,op3));
-         dp[i][j]=max+grid[i][j];
-
-         return dp[i][j];
-    }
-
     public static int goldr(int i, int j, int[][] grid){
         if(i<0 ||i>=grid.length || j>=grid[0].length)
          return 0; 
@@ -30,6 +14,63 @@ public class goldmine {
          max=max+grid[i][j];
 
          return max;
+    }
+
+    public static int goldm(int i, int j, int[][] grid, int[][] dp){
+        if(i<0 ||i>=grid.length || j>=grid[0].length)
+         return 0; 
+         if(dp[i][j]!=0){
+             return dp[i][j];
+         }
+         int op1=goldm(i-1,j+1,grid,dp);
+         int op2=goldm(i,j+1,grid,dp);
+         int op3=goldm(i+1,j+1,grid,dp);
+
+         int max= Math.max(op1,Math.max(op2,op3));
+         dp[i][j]=max+grid[i][j];
+
+         return dp[i][j];
+    }
+    
+    public static int goldt(int[][] arr,int[][] dp){
+        int n=arr.length;
+        int m =arr[0].length;
+        
+        for(int j=m-1;j>=0;j--){
+        for(int i=0;i<n;i++){
+                // if(i<0 ||i>=n || j>=m || j<0)
+                // //dp[i][j]=0;
+                // continue; 
+                
+                if(j==m-1 ){
+                    dp[i][j]=arr[i][j];
+                }
+                else if(i==0){
+                    int max= Math.max(dp[i][j+1],dp[i+1][j+1]);
+                    dp[i][j]=arr[i][j]+max;
+                }
+                
+                else if(i==n-1){
+                    int max = Math.max(dp[i][j+1],dp[i-1][j+1]);
+                    dp[i][j]=arr[i][j]+max;
+                }
+                else{
+                    int max = Math.max(dp[i][j+1],Math.max(dp[i-1][j+1],dp[i+1][j+1]));
+                    dp[i][j]=arr[i][j]+max;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            //System.out.println(dp[i][0]);
+            if(ans<dp[i][0]){
+                
+                ans=dp[i][0];
+            }
+        }
+        return ans;
+
+        // return dp[sr][sc];
     }
 
     public static void main(String[] args) throws Exception {
@@ -45,13 +86,16 @@ public class goldmine {
         }
         int[][] dp=new int[n][m];
         int ans=0;
-        for(int i=0;i<n;i++){
-            int temp=goldm(i,0,arr,dp);
-            if(temp>ans){
-                ans=temp;
-            }
-        }
-        System.out.println(ans);
+        // for(int i=0;i<n;i++){
+        //     int temp=goldm(i,0,arr,dp);
+        //     if(temp>ans){
+        //         ans=temp;
+        //     }
+        // }
+        
+        //System.out.println(ans);
+        
+        System.out.println(goldt(arr,dp));
     }
 
 }
