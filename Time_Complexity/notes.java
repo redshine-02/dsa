e4d6e99aea05a811fb823e3ec45c7c2b3324d17f
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-03 21:35:02 +0530

    Create notes.java

---
+++ Time_Complexity/notes.java
@@ -0,0 +1,16 @@
+public class notes {
+    /*
+    O---> Big oh (Worst case)
+    O(1)----> constant e.g: int a = n; ------->O(1)------> kuchh constant kaam ho rha hai 
+    O(n)---------> when the program takes n traversals it depends on user input
+    Nested loops that is loop within a loop have generally a convention of O(n^2) but it is not
+    necessary that it will happen always open your eyes and have a proper look
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+            System.out.println(arr[i][j]);
+        } // This loop runs always 3 for every value of i
+    }// This loop will run n times
+
+    Total no. of iterations = 3*n-----------> O(3n)==O(n)
+    */
+}
