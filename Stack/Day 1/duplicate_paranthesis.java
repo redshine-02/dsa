3635998b33c6def63e11ca6163b3ebd744f48e33
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-15 16:11:03 +0530

    Create duplicate_paranthesis.java

---
+++ Stack/Day 1/duplicate_paranthesis.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class duplicate_paranthesis {
+    public static boolean balanced(String s){
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i<s.length() ; i++){
+            char ch = s.charAt(i);
+            
+            if( ch != ')'){
+                st.push(ch);
+            }
+            else{
+                if(st.peek()=='('){
+                    return true;
+                }
+                while(st.peek()!='('){
+                    st.pop();
+                }
+                if(st.peek()=='('){
+                    st.pop();
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(balanced(s));
+    }
+
+}
