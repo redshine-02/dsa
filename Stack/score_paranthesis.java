d291528738c06a0e89b66aff2ce1ecedf33a2c5c
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-04-24 19:11:37 +0530

    updates

---
+++ Stack/score_paranthesis.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    // public int scoreOfParentheses(String s) {
+    //     Stack<Integer> st = new Stack<>();
+    //     int count = 0 ;
+    //     for(int i = 0 ; i < s.length() ; i++){
+    //         char ch = s.charAt(i);
+            
+    //         if(ch == '('){
+    //             st.push(0);
+    //         }
+    //         else{
+    //             if(st.peek()==0){
+    //                 st.pop();
+    //                 st.push(1);
+    //             }
+    //             else{
+    //                 int val = 0 ;
+    //                 while(st.peek() != 0){
+    //                     val += st.pop();
+    //                 }
+    //                 st.pop();
+    //                 st.push(2*val);
+    //             }
+    //         }
+    //     }
+        
+    //     while(st.size()>0){
+    //         count+=st.pop();
+    //     }
+    //     return count;
+    // }
+    
+}
