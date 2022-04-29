a8dbe78090b38271fd21fe591fc0d89202164f6f
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-04-29 18:54:38 +0530

    generic trees added

---
+++ Binary_Search_Tree/search.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}
+public class search {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }
+        if(root.val==val){
+            return root;
+        }
+        else if(root.val<val){
+            return searchBST(root.right,val);
+        }
+        else{
+            return searchBST(root.left,val);
+        }
+        
+       // return null;
+        
+    }
+}
