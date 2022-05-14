73a7584c9ecd3b3df5d02510da16297454d6de74
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-05-14 12:49:49 +0530

    Update count_no_text.java

--- Level 2/Recursion/Coin Change Recursion/count_no_text.java
+++ Level 2/Recursion/Coin Change Recursion/count_no_text.java
@@ -1,5 +1,33 @@
 import java.util.*;
 public class count_no_text {
+//     public int count(String s , int idx , int[] dp){
+//         if(idx == s.length()){
+//             return dp[idx]=1;
+//         }
+//         if(dp[idx]!=-1){
+//             return dp[idx];
+//         }
+//         int c = 0 ;
+//         int ch = s.charAt(idx) - '0';
+//         int jump = 3;
+//         if(ch == 7 || ch == 9){
+//             jump++;
+//         }
+//         for(int i = 0 ; i<jump && idx+i<s.length() && s.charAt(idx) == s.charAt(idx+i) ; i++ ){
+//             c += count(s,idx+i+1,dp);
+//             c %= 1000000007;
+//         }
+//          return dp[idx] = c;
+//     }
+   
+
+// public int countTexts(String pressedKeys) {
+//     int n = pressedKeys.length();
+//     int[] dp = new int[n+1];
+//     Arrays.fill(dp,-1);
+//     int ans = count(pressedKeys,0,dp);
+//     return dp[0];
+// }
     public int countTexts(String pressedKeys) {
         int n = pressedKeys.length();
         long[] dp = new long[n+1];
